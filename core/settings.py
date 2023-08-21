d419e9a675a63779f6dd54287bf17ad028541844
mehmanmehman182@gmail.com <X64If#94>
2023-08-21 12:10:50 +0400

    Serializers

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'books.apps.BooksConfig'
+    # 3rd party
+    'rest_framework',
+    #Local
+    'books.apps.BooksConfig',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
